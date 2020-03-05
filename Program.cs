5a676fc2d4aa7d66f23b549add2a906fd58c242b
Eugene <61139973+eugeheless@users.noreply.github.com>
2020-03-05 20:07:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            string[] books = new string[0];
+            Console.WriteLine("Добро пожаловать в обитель хикки-задрота \nТы можешь: \nДобавить книгу - клацни 1\nПосмотреть книги - клацни 2");
+            while (true)
+            {
+                Console.WriteLine("Что делаем?");
+                string choose = Console.ReadLine();
+                if (choose == "1")
+                {
+                    Console.WriteLine("Что добавляем, семпай?");
+                    string add = Console.ReadLine();
+                    books = Add(books, add);
+                    
+                    Console.WriteLine("Добавлено, беги, смотри");
+                }
+                if (choose == "2")
+                {
+                    Console.WriteLine("Пожалуйста, выбирай :3\n");
+                    Show(books);
+                }
+            }
+        }
+        static string[] Add(string[] array, string newValue)
+        {
+            
+            int newLength = array.Length + 1;
+
+            string[] result = new string[newLength];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                result[i] = array[i];
+            }
+            result[newLength - 1] = newValue;
+            return result;
+        }
+       
+        static void Show(string[] array)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Array.Sort(array);
+            for(int i = 0; i< array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+    }
+}
